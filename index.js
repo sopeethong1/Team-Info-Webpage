9f62af255e8e6a6be6903bada3f22d585aa3a29f
Sopee Thong <tsopee@gmail.com>
2021-04-13 00:44:20 -0400

    adding test files

---
+++ index.js
@@ -0,0 +1,6 @@
+//Inquier goes here
+//fs writefile stuff goes here too
+//main logic lives here 
+//index.js file is considered the entry point to your application
+//the dist folder is where the output html will land. the writefile will end up in dist folder (const path from slack)
+//require that page-template.js= const pageTemplate = require(`./src/page-template.js`);
