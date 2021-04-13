a07ab387d6ea8b9654156547bf6703a8d1a27caf
Sopee Thong <tsopee@gmail.com>
2021-04-13 00:58:23 -0400

    adding boilerplate

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 //Inquier goes here
+// we would 1st require ALL of the files and node packages needed 
+//Engineer, Manager, Intern
+//the above extend from the main class Employee
 //fs writefile stuff goes here too
 //main logic lives here 
 //index.js file is considered the entry point to your application
 //the dist folder is where the output html will land. the writefile will end up in dist folder (const path from slack)
-//require that page-template.js= const pageTemplate = require(`./src/page-template.js`);
+//We are receiving that anonymous function 
+//Gibing the name of pageTemplate
+// require that page-template.js= 
+const pageTemplate = require(`./src/page-template.js`);
+//and now, we can use that pageTemplate as a function, which can ACCEPT a parameter
+pageTemplate(answer_from_inquirer_prompt);
+
---
+++ src/page-template.js
@@ -0,0 +1,43 @@
+// generate the HTML pages
+const makeTeam = team => {
+//a method for a template to render manager info
+
+//a method for a template to render engineer info
+
+// a method for a template to render intern info
+
+
+    const  generateManager = manager = {
+        ` return ${manager.getName()} etc etc`
+    }
+    const html = [];
+        html.push(team
+            .filter(employee => employee.getRole() === "Manager")
+            .map(manager => generateManager(manager))
+        );
+        return html.join("");
+
+}
+//We are exporting out an anonymous function
+
+module.exports = team => {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        YOUR METHOD GOES HERE THE CARDS OF MANAGER, INTERN, ENGINEER, WILL BE 
+        ${someFunction(team)}
+    </body>
+    </html>
+    
+    `
+}
+
+
+
+
